77e17ec914007abc203019900b9b05fb4019ef87
Elizaveta Bondarenko <libondarenko10@gmail.com>
2025-12-27 14:56:03 +0100

    Controlling bookingSrvice

--- src/main/java/cz/cvut/fel/ear/projekt/service/UserService.java
+++ src/main/java/cz/cvut/fel/ear/projekt/service/UserService.java
@@ -1,23 +1,23 @@
-package cz.cvut.fel.ear.projekt.service;
-
-import cz.cvut.fel.ear.projekt.dao.UserDao;
-import cz.cvut.fel.ear.projekt.model.User;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
-
-@Service
-@Transactional
-public class UserService {
-    private final UserDao userDao;
-
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
-    }
-    public void createUser(User user) {
-        Objects.requireNonNull(user, "user is required");
-        user.validateAdult();
-        userDao.save(user);
-    }
-}
+//package cz.cvut.fel.ear.projekt.service;
+//
+//import cz.cvut.fel.ear.projekt.dao.UserDao;
+//import cz.cvut.fel.ear.projekt.model.User;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.Objects;
+//
+//@Service
+//@Transactional
+//public class UserService {
+//    private final UserDao userDao;
+//
+//    public UserService(UserDao userDao) {
+//        this.userDao = userDao;
+//    }
+//    public void createUser(User user) {
+//        Objects.requireNonNull(user, "user is required");
+//        user.validateAdult();
+//        userDao.save(user);
+//    }
+//}
